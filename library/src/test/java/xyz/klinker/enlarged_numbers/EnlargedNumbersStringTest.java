c3f43589c0b49e4e40b64bb0cb8dcb8ef85b56a1
Luke Klinker <lklinker1@gmail.com>
2016-12-22 16:27:58 -0600

    bring in robolectric to finish the unit test

--- library/src/test/java/xyz/klinker/enlarged_numbers/EnlargedNumbersStringTest.java
+++ library/src/test/java/xyz/klinker/enlarged_numbers/EnlargedNumbersStringTest.java
@@ -1,16 +1,29 @@
 package xyz.klinker.enlarged_numbers;
 
 import android.text.style.RelativeSizeSpan;
-import org.junit.Test;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
 
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class EnlargedNumbersStringTest {
+
+    private EnlargedNumbersString spannable;
+
+    @Before
+    public void setUp() {
+        spannable = spy(EnlargedNumbersFactory.build("1.001 MB"));
+    }
 
     @Test
     public void test_matcherLength() {
-        EnlargedNumbersString spannable = EnlargedNumbersFactory.build("test");
-
         assertEquals(0, spannable.findNumbers("test").size());
         assertEquals(1, spannable.findNumbers("1.").size());
         assertEquals(2, spannable.findNumbers("1. 01").size());
@@ -23,10 +36,9 @@ public class EnlargedNumbersStringTest {
 
     @Test
     public void test_spannableType() {
-        EnlargedNumbersString spannable = EnlargedNumbersFactory.build("1.001 MB");
         assertEquals(1, spannable.getSpans(0, 8, RelativeSizeSpan.class).length);
 
-        spannable = EnlargedNumbersFactory.build("1.001 KB 11");
+        spannable = spy(EnlargedNumbersFactory.build("1.001 KB 11"));
         assertEquals(2, spannable.getSpans(0,11, RelativeSizeSpan.class).length);
     }
 
